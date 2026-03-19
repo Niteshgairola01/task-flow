9bce0771e9af95bbbb642231f50ea724aa9dc44a
Niteshgairola01 <niteshgairolapapl@gmail.com>
2026-03-19 10:33:44 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className='text-white min-w-screen min-h-screen bg-blue-900'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, architecto!
+      </div>
+    </>
+  )
+}
+
+export default App
